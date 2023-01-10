0ba611c617f61aaf97effd8f915dc872561e7bf6
wissem <saadliwissem88@gmail.com>
2023-01-10 23:25:01 +0100

    redirect file added

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
 import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
