ecc4b90cf2b585c8a8496bc8b8bfc086c19c188f
wissem <saadliwissem88@gmail.com>
2023-01-14 21:18:16 +0100

    last commit

--- pages/index.js
+++ pages/index.js
@@ -216,7 +216,7 @@ export default function Home() {
           </div>
         </section>
         <section>
-          footer
+        <h4 className="py-4 text-teal-600">footer </h4>
         </section>
       </main>
     </div>
